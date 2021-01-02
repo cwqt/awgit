0bd6763232e5b9ba5b1dd54974b6f66101096b4a
Cass <dev@cass.si>
2021-01-02 13:07:53 +0000

    fix DEFAULT app already exists

--- helpers/firestore.ts
+++ helpers/firestore.ts
@@ -6,16 +6,19 @@ export type Collection = firebase.firestore.CollectionReference<firebase.firesto
 export type FireStore = firebase.firestore.Firestore;
 
 const setup = async ():Promise<[FireStore, Collection]> => {
-  const app = firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: 'awgit-38dbb.firebaseapp.com',
-    projectId: 'awgit-38dbb',
-    storageBucket: 'awgit-38dbb.appspot.com',
-    messagingSenderId: '377518986163',
-    appId: '1:377518986163:web:0bacd2d962b09fbab6b1c6',
-    measurementId: 'G-6LVJX85VVB',
-  });
+  if(firebase.apps.length === 0){
+    firebase.initializeApp({
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: 'awgit-38dbb.firebaseapp.com',
+      projectId: 'awgit-38dbb',
+      storageBucket: 'awgit-38dbb.appspot.com',
+      messagingSenderId: '377518986163',
+      appId: '1:377518986163:web:0bacd2d962b09fbab6b1c6',
+      measurementId: 'G-6LVJX85VVB',
+    });
+  }
 
+  const app = firebase.app();
   await app
     .auth()
     .signInWithEmailAndPassword(process.env.FIREBASE_EMAIL, process.env.FIREBASE_PASSWORD);
